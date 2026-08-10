9705c109eef971fffece2de7fe0b49fbcb5393da
rajahpixelphone <rajahpixelphone@gmail.com>
2026-08-10 23:12:50 +0530

    Add main Spring Boot application class

---
+++ backend/src/main/java/com/scoops/brainfreeze/ScoopsOfBrainfreezeApplication.java
@@ -0,0 +1,12 @@
+package com.scoops.brainfreeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoopsOfBrainfreezeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoopsOfBrainfreezeApplication.class, args);
+    }
+}
